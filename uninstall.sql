1e462dca467998b7c9c59a629590ac10e818dcd6
Dini4K <office@prime-tsk.ru>
2025-09-27 16:29:59 +0300

    Delete uninstall.sql

--- uninstall.sql
+++
@@ -1,5 +0,0 @@
-DROP TABLE IF EXISTS `{{DB_PREFIX}}shtab_setting`;
-DROP TABLE IF EXISTS `{{DB_PREFIX}}shtab_log`;
-DROP TABLE IF EXISTS `{{DB_PREFIX}}shtab_product`;
-DROP TABLE IF EXISTS `{{DB_PREFIX}}shtab_product_to_marketplace`;
-DROP TABLE IF EXISTS `{{DB_PREFIX}}shtab_competitor_price`;
